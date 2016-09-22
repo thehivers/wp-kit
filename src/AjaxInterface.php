68c759b8092b702936172f9286e89a0802b92044
Nikola Kanacki <mail@nikolakanacki.com>
2016-09-22 23:30:24 +0200

    feat: More reliable AjaxInterface

--- src/AjaxInterface.php
+++ src/AjaxInterface.php
@@ -6,8 +6,12 @@ class AjaxInterface
 {
   use \OhLabs\WPKit\Traits\Singleton;
   
+  /** @const string Default error */
+  const DEFAULT_ERROR = 'Oops! An error has occured while getting your request. Please try again.';
+  /** @const string Default success */
+  const DEFAULT_SUCCESS = 'All good.';
   /** @var array Response messages (code => message) */
-  protected $responses = array (100 => 'All good.');
+  protected $responses = array (0=>'All good.');
   
   /**
    * Initialize by handling request
@@ -44,11 +48,38 @@ class AjaxInterface
   }
   
   /**
+   * Get file value
+   */
+  public function fileVal ($name)
+  {
+    if (isset($_FILES[$name])) return $_FILES[$name];
+    return null;
+  }
+  
+  /**
    * Die with response code
    */
-  public function end ($code=200)
+  public function end ($code=0)
   {
-    $this->json(array('code'=>$code,'message'=>$this->responses[$code]));
+    $res = array('error'=>false,'code'=>$code);
+    $res['message'] = isset($this->responses[$code])
+    ? $this->responses[$code]
+    : $code > 0
+    ? static::DEFAULT_ERROR
+    : static::DEFAULT_SUCCESS;
+    $this->json(array('code'=>$code));
+  }
+  
+  /**
+   * Die with error code
+   */
+  public function error ($code=1,$message=null)
+  {
+    $res = array('error'=>true,'code'=>$code);
+    $res['message'] = is_string($message)
+    ? $message
+    : static::DEFAULT_ERROR;
+    $this->json($res);
   }
   
   /**
