9f8b06366d530961947089224b23ab957f0ecf73
Nikola Kanacki <mail@nikolakanacki.com>
2016-10-18 08:29:03 +0200

    feat: introducing ACF.php

---
+++ src/ACF.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace OhLabs\WPKit;
+
+class ACF
+{
+  use \OhLabs\WPKit\Traits\Singleton;
+
+  /** Unique key holder */
+  private $key = 0;
+
+  /** Unique key generator */
+  private function key ($base='')
+  {
+    $k = $this->key++;
+    return $base . $k;
+  }
+
+  /**
+   * Use configuration file to register fields
+   */
+  public function useConfig ($path)
+  {
+    if (file_exists($path)) require $path;
+  }
+
+  /**
+   * Register acf group
+   */
+  public function addGroup ($conf,$location)
+  {
+    $key  = $this->key('group_');
+    $args = array_merge(array(
+      'key' => $key,
+      'location' => $location
+    ),$conf);
+    acf_add_local_field_group($args);
+    return $args['key'];
+  }
+
+  /**
+   * Register acf field
+   */
+  public function addField ($parent,$label,$name,$type,$required=0,$conf=array())
+  {
+    $key  = $this->key('field_');
+    $args = array_merge(array(
+      'key' => $key,
+      'parent' => $parent,
+      'label' => $label,
+      'name' => $name,
+      'type' => $type
+    ),$conf);
+    acf_add_local_field ($args);
+    return $args['key'];
+  }
+}
