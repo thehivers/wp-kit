d3e7d804c71cf6df6437ae5a2064bc2cd2dbd729
Nikola Kanacki <mail@nikolakanacki.com>
2016-11-28 23:00:47 +0100

    chore: Dependencies class is now based on admin_notices and not plugin_action_links

--- src/Dependencies.php
+++ src/Dependencies.php
@@ -6,14 +6,11 @@ class Dependencies
 {
   use \OhLabs\WPKit\Traits\Singleton;
   use \OhLabs\WPKit\Traits\Debug;
-  use \OhLabs\WPKit\Traits\PluginActions;
 
   /** @var string The class name of the main plugin that requires dependencies */
   protected $plugin;
   /** @var array  Plugin paths to look for with their download locations */
   protected $dependencies = array();
-  /** @var string Admin post action used for plugin action link */
-  protected $adminPostAction = 'wpkit-install-dependencies';
   /** @var array  Cache missing dependencies */
   protected $missing;
 
@@ -22,37 +19,59 @@ class Dependencies
    */
   protected function __construct ()
   {
-    $this->usePluginActions();
+    add_action (
+      'admin_post_wpkit-setup-missing-dependencies',
+      array($this,'_setup_missing_dependencies')
+    );
+  }
+
+  /**
+   * Handle admin post request
+   * @private
+   */
+  public function _setup_missing_dependencies ()
+  {
+    $this->setupMissingDependencies();
+    die(wp_redirect(admin_url('plugins.php')));
   }
 
   /**
    * Insert a plugin action link if there are unmet dependencies
    * @private
    */
-  public function pluginActionLinks ($links)
+  public function issueAdminNotice ()
   {
-    if (!$this->areMet()) {
-      $links[] = $this->renderPluginActionLink('Setup Dependencies');
-    }
-    return $links;
+    add_action ('admin_notices',array($this,'renderAdminNotice'));
+  }
+
+  /**
+   * Render admin notice
+   */
+  public function renderAdminNotice ()
+  {
+    echo '<div class="notice notice-error">';
+    echo '<p>System setup could not complete, please click <a href="';
+    echo admin_url('admin-post.php?action=wpkit-setup-missing-dependencies');
+    echo '">here</a> to install the missing dependencies.</p>';
+    echo '</div>';
   }
 
   /**
    * Install and activate missing dependencies
    * @private
    */
-  public function pluginActionCallback ()
+  public function setupMissingDependencies ()
   {
     if (!current_user_can('manage_options')) {
-      $this->pluginActionCallbackEnd();
+      return false;
     }
-    $missing = $this->getMissing();
+    $missing = $this->getMissingDependencies();
     if (empty($missing)) {
-      $this->pluginActionCallbackEnd();
+      return false;
     }
     @include_once (ABSPATH . 'wp-admin/includes/plugin.php');
     if (!function_exists('activate_plugin')) {
-      $this->pluginActionCallbackEnd();
+      return false;
     }
     foreach ($missing as $path => $src) {
       if (!@file_exists(WP_PLUGIN_DIR . '/' . $path)) {
@@ -70,13 +89,13 @@ class Dependencies
       }
       activate_plugin ($path);
     }
-    $this->pluginActionCallbackEnd();
+    return true;
   }
 
   /**
    * Get missing dependencies
    */
-  public function getMissing ()
+  public function getMissingDependencies ()
   {
     if (isset($this->missing)) {
       return $this->missing;
@@ -105,7 +124,7 @@ class Dependencies
    */
   public function areMet ()
   {
-    $missing = $this->getMissing();
+    $missing = $this->getMissingDependencies();
     return empty($missing);
   }
 }
